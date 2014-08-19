c00429c4d2e266248fd6772826cf3572461feec8
Jared King <j@jaredtking.com>
2014-08-19 16:20:14 -0500

    import from framework

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-framework-iron-mq
+framework-iron-mq [![Build Status](https://travis-ci.org/idealistsoft/framework-iron-mq.png?branch=master)](https://travis-ci.org/idealistsoft/framework-iron-mq)
 =================
 
+[![Coverage Status](https://coveralls.io/repos/idealistsoft/framework-iron-mq/badge.png)](https://coveralls.io/r/idealistsoft/framework-iron-mq)
+[![Latest Stable Version](https://poser.pugx.org/idealistsoft/framework-iron-mq/v/stable.png)](https://packagist.org/packages/idealistsoft/framework-iron-mq)
+[![Total Downloads](https://poser.pugx.org/idealistsoft/framework-iron-mq/downloads.png)](https://packagist.org/packages/idealistsoft/framework-iron-mq)
+
 IronMQ module for Idealist Framework
+
+## Installation
+
+Add the composer package in the require section of your app's `composer.json` and run `composer update`
---
+++ src/app/iron/Controller.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\iron;
+
+use App;
+
+class Controller
+{
+	public static $properties = [
+		'routes' => [
+			'post /iron/message' => 'message',
+			'get /iron/install' => 'setupQueues',
+			'get /iron/processQueues' => 'processQueues',
+		]
+	];
+
+	private $app;
+
+	function __construct( App $app )
+	{
+		$this->app = $app;
+	}
+
+	function message( $req, $res )
+	{
+		// verify auth token
+		if( $req->query( 'auth' ) != $this->app[ 'config' ]->get( 'iron.auth_token' ) )
+			return $res->setCode( 401 );
+
+		// construct the message from the request
+		$message = new \stdClass;
+		$message->id = $req->headers( 'Iron_Message_Id' );
+		$message->body = json_decode( $req->request() );
+
+		// grab the message from the post body
+		$this->app[ 'queue' ]->receiveMessage( $req->query( 'q' ), $message );
+
+		$res->setCode( 200 );
+	}
+
+    function setupQueues( $req, $res )
+    {
+    	if( !$req->isCli() )
+    		return $res->setCode( 404 );
+
+    	if( $this->app[ 'queue' ]->install() )
+    	{
+    		foreach( $this->app[ 'queue' ]->subscribers() as $q => $subscribers )
+    		{
+				echo "Installed $q with subscribers:\n";
+				print_r( $subscribers );
+    		}
+    	}
+    }
+
+    function processQueues( $req, $res )
+    {
+    	if( !$req->isCli() )
+    		return $res->setCode( 404 );
+
+		foreach( $this->app[ 'config' ]->get( 'queue.queues' ) as $q )
+		{
+			echo "Processing messages for $q queue:\n";
+
+			$messages = $this->app[ 'queue' ]->dequeue( $q, 10 );
+
+			print_r( $messages );
+
+			foreach( (array)$messages as $message )
+				$this->app[ 'queue' ]->receiveMessage( $q, $message );
+		}
+    }
+}
