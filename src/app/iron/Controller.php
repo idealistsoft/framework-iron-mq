f1c4bbc5dcb2eb57ab3cca51688f2f41a867ec4b
Jared King <j@jaredtking.com>
2014-09-02 10:43:57 -0500

    implement psr-1 and psr-2 coding styles via php-cs-fixer

--- src/app/iron/Controller.php
+++ src/app/iron/Controller.php
@@ -8,75 +8,75 @@ use App;
 
 class Controller
 {
-	public static $properties = [
-		'routes' => [
-			'post /iron/message' => 'message',
-			'get /iron/install' => 'setupQueues',
-			'get /iron/processQueues' => 'processQueues',
-		]
-	];
+    public static $properties = [
+        'routes' => [
+            'post /iron/message' => 'message',
+            'get /iron/install' => 'setupQueues',
+            'get /iron/processQueues' => 'processQueues',
+        ]
+    ];
 
-	private $app;
+    private $app;
 
-	function __construct( App $app )
-	{
-		$this->app = $app;
-	}
-
-	function middleware( $req, $res )
-	{
-		$this->app[ 'ironmq' ] = function( $c ) {
-			return new IronMQ( $c[ 'config' ]->get( 'ironmq' ) );
-		};
-	}
-
-	function message( $req, $res )
-	{
-		// verify auth token
-		if( $req->query( 'auth' ) != $this->app[ 'config' ]->get( 'iron.auth_token' ) )
-			return $res->setCode( 401 );
-
-		// construct the message from the request
-		$message = new \stdClass;
-		$message->id = $req->headers( 'Iron_Message_Id' );
-		$message->body = json_decode( $req->request() );
-
-		// grab the message from the post body
-		$this->app[ 'queue' ]->receiveMessage( $req->query( 'q' ), $message );
-
-		$res->setCode( 200 );
-	}
-
-    function setupQueues( $req, $res )
+    public function __construct(App $app)
     {
-    	if( !$req->isCli() )
-    		return $res->setCode( 404 );
-
-    	if( $this->app[ 'queue' ]->install() )
-    	{
-    		foreach( $this->app[ 'queue' ]->pushQueueSubscribers() as $q => $subscribers )
-    		{
-				echo "Installed $q with subscribers:\n";
-				print_r( $subscribers );
-    		}
-    	}
+        $this->app = $app;
     }
 
-    function processQueues( $req, $res )
+    public function middleware($req, $res)
     {
-    	if( !$req->isCli() )
-    		return $res->setCode( 404 );
+        $this->app[ 'ironmq' ] = function ($c) {
+            return new IronMQ( $c[ 'config' ]->get( 'ironmq' ) );
+        };
+    }
 
-		foreach( $this->app[ 'config' ]->get( 'queue.queues' ) as $q )
-		{
-			echo "Processing messages for $q queue:\n";
+    public function message($req, $res)
+    {
+        // verify auth token
+        if( $req->query( 'auth' ) != $this->app[ 'config' ]->get( 'iron.auth_token' ) )
 
-			$messages = $this->app[ 'queue' ]->dequeue( $q, 10 );
+            return $res->setCode( 401 );
 
-			print_r( $messages );
+        // construct the message from the request
+        $message = new \stdClass();
+        $message->id = $req->headers( 'Iron_Message_Id' );
+        $message->body = json_decode( $req->request() );
 
-			foreach( (array)$messages as $message )
-				$this->app[ 'queue' ]->receiveMessage( $q, $message );
-		}
+        // grab the message from the post body
+        $this->app[ 'queue' ]->receiveMessage( $req->query( 'q' ), $message );
+
+        $res->setCode( 200 );
+    }
+
+    public function setupQueues($req, $res)
+    {
+        if( !$req->isCli() )
+
+            return $res->setCode( 404 );
+
+        if ( $this->app[ 'queue' ]->install() ) {
+            foreach ( $this->app[ 'queue' ]->pushQueueSubscribers() as $q => $subscribers ) {
+                echo "Installed $q with subscribers:\n";
+                print_r( $subscribers );
+            }
+        }
+    }
+
+    public function processQueues($req, $res)
+    {
+        if( !$req->isCli() )
+
+            return $res->setCode( 404 );
+
+        foreach ( $this->app[ 'config' ]->get( 'queue.queues' ) as $q ) {
+            echo "Processing messages for $q queue:\n";
+
+            $messages = $this->app[ 'queue' ]->dequeue( $q, 10 );
+
+            print_r( $messages );
+
+            foreach( (array) $messages as $message )
+                $this->app[ 'queue' ]->receiveMessage( $q, $message );
+        }
     }
 }
