f14d7149402cdbd27c6f52cab10d3cebf2e90fce
Jared King <j@jaredtking.com>
2014-11-29 20:05:41 -0600

    cs

--- src/Controller.php
+++ src/Controller.php
@@ -13,63 +13,64 @@ class Controller
             'post /iron/message' => 'message',
             'get /iron/install' => 'setupQueues',
             'get /iron/processQueues' => 'processQueues',
-        ]
+        ],
     ];
 
     public function middleware($req, $res)
     {
         $this->app[ 'ironmq' ] = function ($c) {
-            return new IronMQ( $c[ 'config' ]->get( 'ironmq' ) );
+            return new IronMQ($c[ 'config' ]->get('ironmq'));
         };
     }
 
     public function message($req, $res)
     {
         // verify auth token
-        if( $req->query( 'auth' ) != $this->app[ 'config' ]->get( 'iron.auth_token' ) )
-
-            return $res->setCode( 401 );
+        if ($req->query('auth') != $this->app[ 'config' ]->get('iron.auth_token')) {
+            return $res->setCode(401);
+        }
 
         // construct the message from the request
         $message = new \stdClass();
-        $message->id = $req->headers( 'Iron_Message_Id' );
-        $message->body = json_decode( $req->request() );
+        $message->id = $req->headers('Iron_Message_Id');
+        $message->body = json_decode($req->request());
 
         // grab the message from the post body
-        $this->app[ 'queue' ]->receiveMessage( $req->query( 'q' ), $message );
+        $this->app[ 'queue' ]->receiveMessage($req->query('q'), $message);
 
-        $res->setCode( 200 );
+        $res->setCode(200);
     }
 
     public function setupQueues($req, $res)
     {
-        if( !$req->isCli() )
+        if (!$req->isCli()) {
+            return $res->setCode(404);
+        }
 
-            return $res->setCode( 404 );
-
-        if ( $this->app[ 'queue' ]->install() ) {
-            foreach ( $this->app[ 'queue' ]->pushQueueSubscribers() as $q => $subscribers ) {
+        if ($this->app[ 'queue' ]->install()) {
+            foreach ($this->app[ 'queue' ]->pushQueueSubscribers() as $q => $subscribers) {
                 echo "Installed $q with subscribers:\n";
-                print_r( $subscribers );
+                print_r($subscribers);
             }
         }
     }
 
     public function processQueues($req, $res)
     {
-        if( !$req->isCli() )
+        if (!$req->isCli()) {
+            return $res->setCode(404);
+        }
 
-            return $res->setCode( 404 );
-
-        foreach ( $this->app[ 'config' ]->get( 'queue.queues' ) as $q ) {
+        foreach ($this->app[ 'config' ]->get('queue.queues') as $q) {
             echo "Processing messages for $q queue:\n";
 
-            $messages = $this->app[ 'queue' ]->dequeue( $q, 10 );
+            $messages = $this->app[ 'queue' ]->dequeue($q, 10);
 
-            print_r( $messages );
+            print_r($messages);
 
-            foreach( (array) $messages as $message )
-                $this->app[ 'queue' ]->receiveMessage( $q, $message );
+            foreach ((array) $messages as $message) {
+                $this->app[ 'queue' ]->receiveMessage($q, $message);
+            }
         }
     }
 }
